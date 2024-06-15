a634053519e9afe4d77376626e30e5bb8c23277b
Plixio <raflyadie1407@gmail.com>
2024-06-15 18:11:34 +0700

    memperbaiki error

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const mysql2 = require('mysql2');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: mysql2,
 });
 
 module.exports = sequelize;
