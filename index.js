74749ff667abe03ee5c8a27c3fff0af91ed4d756
Plixio <raflyadie1407@gmail.com>
2024-06-15 17:14:45 +0700

    menambahkan semuanya

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require('express');
-const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sequelize = require('./config/database');
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Halo dari Express di Vercel!');
-});
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const coupleRouter = require('./routes/couple');
+const eventRouter = require('./routes/event');
+const visitorRouter = require('./routes/visitor');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api', usersRouter);
+app.use('/api', coupleRouter);
+app.use('/api', eventRouter);
+app.use('/api', visitorRouter);
+
+sequelize.sync()
+    .then(() => {
+        console.log('Databases synced!');
+    })
+    .catch(error => {
+        console.error('Unabled to sync the database:', error);
+    });
 
 app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
